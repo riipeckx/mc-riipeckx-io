50a57ce82492863893c5d1493b029770bb34b300
riipeckx <riipeckx@linux.com>
2024-10-25 13:31:23 +0200

    feat: :sparkles: update start script

--- start.sh
+++ start.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# java $JAVAFLAGS -jar server.jar --nogui
-
 JAVAFLAGS="-Xms3072M \
-Xmx3072M \
+-Xmx3072M \
 --add-modules=jdk.incubator.vector \
 -XX:+UseG1GC \
 -XX:+ParallelRefProcEnabled \
@@ -14,8 +12,8 @@ Xmx3072M \
 -XX:G1HeapWastePercent=5 \
 -XX:G1MixedGCCountTarget=4 \
 -XX:InitiatingHeapOccupancyPercent=15 \
--XX:G1MixedGCLiveThresholdPercent=90 
-\-XX:G1RSetUpdatingPauseTimePercent=5 \
+-XX:G1MixedGCLiveThresholdPercent=90 \
+-XX:G1RSetUpdatingPauseTimePercent=5 \
 -XX:SurvivorRatio=32 \
 -XX:+PerfDisableSharedMem \
 -XX:MaxTenuringThreshold=1 \
@@ -25,3 +23,5 @@ Xmx3072M \
 -XX:G1MaxNewSizePercent=40 \
 -XX:G1HeapRegionSize=8M \
 -XX:G1ReservePercent=20"
+
+java "$JAVAFLAGS" -jar server.jar --nogui
